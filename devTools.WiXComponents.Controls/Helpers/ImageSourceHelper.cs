a438aa8c9827f5d32aba0346614d345b0a51006a
asm <ahmed.assem@flairstech.com>
2021-12-15 19:44:22 +0200

    Fix stupidity

---
+++ devTools.WiXComponents.Controls/Helpers/ImageSourceHelper.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Concurrent;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using essentialMix;
+
+namespace devTools.WiXComponents.Controls.Helpers
+{
+	public static class ImageSourceHelper
+	{
+		private static readonly ConcurrentDictionary<SHSTOCKICONID, ImageSource> __stockIconCache = new ConcurrentDictionary<SHSTOCKICONID, ImageSource>();
+
+		public static ImageSource FomMessageBoxImage(MessageBoxImage image)
+		{
+			if (image == MessageBoxImage.None) return null;
+			SHSTOCKICONID id = image switch
+			{
+				MessageBoxImage.Error => SHSTOCKICONID.SIID_ERROR,
+				MessageBoxImage.Question => SHSTOCKICONID.SIID_HELP,
+				MessageBoxImage.Warning => SHSTOCKICONID.SIID_WARNING,
+				MessageBoxImage.Information => SHSTOCKICONID.SIID_INFO,
+				_ => throw new ArgumentOutOfRangeException(nameof(image))
+			};
+			return FromSystem(id);
+		}
+
+		public static ImageSource FromSystem(SHSTOCKICONID id)
+		{
+			if (!Enum.IsDefined(typeof(SHSTOCKICONID), id)) throw new ArgumentOutOfRangeException(nameof(id));
+			return __stockIconCache.GetOrAdd(id, k =>
+			{
+				SHSTOCKICONINFO sii = new SHSTOCKICONINFO
+				{
+					cbSize = (uint)Marshal.SizeOf(typeof(SHSTOCKICONINFO))
+				};
+
+				int hr = NativeMethods.SHGetStockIconInfo(k, SHGSI.SHGSI_ICON | SHGSI.SHGSI_SMALLICON, ref sii);
+
+				if (ResultCom.Succeeded(hr))
+				{
+					ImageSource img = Imaging.CreateBitmapSourceFromHIcon(sii.hIcon, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+					NativeMethods.DestroyIcon(sii.hIcon);
+					return img;
+				}
+
+				// try to get it from SystemIcons
+				Icon icon = k switch
+				{
+					SHSTOCKICONID.SIID_APPLICATION => SystemIcons.Application,
+					SHSTOCKICONID.SIID_INFO => SystemIcons.Information,
+					SHSTOCKICONID.SIID_ERROR => SystemIcons.Error,
+					SHSTOCKICONID.SIID_WARNING => SystemIcons.Warning,
+					SHSTOCKICONID.SIID_HELP => SystemIcons.Question,
+					SHSTOCKICONID.SIID_SHIELD => SystemIcons.Shield,
+					_ => null
+				};
+
+				if (icon == null)
+				{
+					Marshal.ThrowExceptionForHR(hr);
+					throw new ArgumentOutOfRangeException(nameof(id));
+				}
+				return Imaging.CreateBitmapSourceFromHIcon(icon.Handle, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+			});
+		}
+	}
+}
---
+++ devTools.WiXComponents.Controls/UACShieldButton.cs
@@ -0,0 +1,111 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using devTools.WiXComponents.Controls.Helpers;
+using essentialMix.Helpers;
+using Image = System.Windows.Controls.Image;
+
+namespace devTools.WiXComponents.Controls
+{
+	/// <inheritdoc cref="Button" />
+	public class UACShieldButton : Button
+	{
+		public static readonly DependencyProperty IconRequestedProperty = DependencyProperty.Register(nameof(IconRequested), 
+																										typeof(bool), 
+																										typeof(UACShieldButton), 
+																										new FrameworkPropertyMetadata(true, 
+																																	FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsRender, 
+																																	OnIconRequestedChanged));
+
+		public static readonly DependencyProperty IconProperty = DependencyProperty.Register(nameof(Icon), 
+																								typeof(ImageSource), 
+																								typeof(UACShieldButton), 
+																								new FrameworkPropertyMetadata(ImageSourceHelper.FromSystem(SHSTOCKICONID.SIID_SHIELD), 
+																															FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsRender, 
+																															OnIconChanged));
+
+		public static DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(UACShieldButton), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+		private readonly Image _image;
+
+		/// <inheritdoc />
+		public UACShieldButton()
+		{
+			_image = new Image
+			{
+				Source = Icon,
+				Margin = new Thickness(0, 0, 4, 0),
+				Stretch = Stretch.None,
+				HorizontalAlignment = HorizontalAlignment.Left,
+				VerticalAlignment = VerticalAlignment.Center,
+				Visibility = IconRequested && !WindowsIdentityHelper.HasElevatedPrivileges()
+								? Visibility.Visible
+								: Visibility.Collapsed
+			};
+			TextBlock text = new TextBlock
+			{
+				HorizontalAlignment = HorizontalAlignment.Stretch,
+				VerticalAlignment = VerticalAlignment.Center
+			};
+			text.SetBinding(TextBlock.TextProperty, new Binding(nameof(Text))
+			{
+				RelativeSource = new RelativeSource
+				{
+					Mode = RelativeSourceMode.FindAncestor,
+					AncestorType = GetType()
+				}
+			});
+			Grid.SetColumn(text, 1);
+			Grid grid = new Grid
+			{
+				ColumnDefinitions =
+				{
+					new ColumnDefinition { Width = new GridLength(1, GridUnitType.Auto) },
+					new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+				},
+				Children =
+				{
+					_image,
+					text
+				}
+			};
+			Content = grid;
+		}
+
+		public bool IconRequested
+		{
+			get => (bool)GetValue(IconRequestedProperty); 
+			set => SetValue(IconRequestedProperty, value);
+		}
+
+		public ImageSource Icon
+		{
+			get => (ImageSource)GetValue(IconProperty); 
+			set => SetValue(IconProperty, value);
+		}
+
+		public string Text
+		{
+			get => (string)GetValue(TextProperty);
+			set => SetValue(TextProperty, value);
+		}
+
+		private static void OnIconRequestedChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+		{
+			UACShieldButton button = (UACShieldButton)sender;
+			button._image.Visibility = (bool)args.NewValue && !WindowsIdentityHelper.HasElevatedPrivileges()
+									? Visibility.Visible
+									: Visibility.Collapsed;
+		}
+
+		private static void OnIconChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+		{
+			UACShieldButton button = (UACShieldButton)sender;
+			button._image.Source = args.NewValue as ImageSource;
+			button._image.Visibility = (bool)sender.GetValue(IconRequestedProperty) && !WindowsIdentityHelper.HasElevatedPrivileges()
+											? Visibility.Visible
+											: Visibility.Collapsed;
+		}
+	}
+}
