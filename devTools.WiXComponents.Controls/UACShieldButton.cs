3e9c5d06af387fa900f7aebcb13d36f456ab9667
asm <asm2025@outlook.com>
2022-02-06 01:36:42 +0200

    Fix merge issues

--- devTools.WiXComponents.Controls/UACShieldButton.cs
+++ devTools.WiXComponents.Controls/UACShieldButton.cs
@@ -11,18 +11,18 @@ namespace devTools.WiXComponents.Controls
 	/// <inheritdoc cref="Button" />
 	public class UACShieldButton : Button
 	{
-		public static readonly DependencyProperty IconRequestedProperty = DependencyProperty.Register(nameof(IconRequested), 
-																										typeof(bool), 
-																										typeof(UACShieldButton), 
-																										new FrameworkPropertyMetadata(true, 
-																																	FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsRender, 
+		public static readonly DependencyProperty IconRequestedProperty = DependencyProperty.Register(nameof(IconRequested),
+																										typeof(bool),
+																										typeof(UACShieldButton),
+																										new FrameworkPropertyMetadata(true,
+																																	FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsRender,
 																																	OnIconRequestedChanged));
 
-		public static readonly DependencyProperty IconProperty = DependencyProperty.Register(nameof(Icon), 
-																								typeof(ImageSource), 
-																								typeof(UACShieldButton), 
-																								new FrameworkPropertyMetadata(ImageSourceHelper.FromSystem(SHSTOCKICONID.SIID_SHIELD), 
-																															FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsRender, 
+		public static readonly DependencyProperty IconProperty = DependencyProperty.Register(nameof(Icon),
+																								typeof(ImageSource),
+																								typeof(UACShieldButton),
+																								new FrameworkPropertyMetadata(ImageSourceHelper.FromSystem(SHSTOCKICONID.SIID_SHIELD),
+																															FrameworkPropertyMetadataOptions.Inherits | FrameworkPropertyMetadataOptions.AffectsRender,
 																															OnIconChanged));
 
 		public static DependencyProperty TextProperty = DependencyProperty.Register(nameof(Text), typeof(string), typeof(UACShieldButton), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
@@ -39,7 +39,7 @@ namespace devTools.WiXComponents.Controls
 				Stretch = Stretch.None,
 				HorizontalAlignment = HorizontalAlignment.Left,
 				VerticalAlignment = VerticalAlignment.Center,
-				Visibility = IconRequested && !WindowsIdentityHelper.HasElevatedPrivileges()
+				Visibility = IconRequested && !WindowsIdentityHelper.HasElevatedPrivileges
 								? Visibility.Visible
 								: Visibility.Collapsed
 			};
@@ -75,13 +75,13 @@ namespace devTools.WiXComponents.Controls
 
 		public bool IconRequested
 		{
-			get => (bool)GetValue(IconRequestedProperty); 
+			get => (bool)GetValue(IconRequestedProperty);
 			set => SetValue(IconRequestedProperty, value);
 		}
 
 		public ImageSource Icon
 		{
-			get => (ImageSource)GetValue(IconProperty); 
+			get => (ImageSource)GetValue(IconProperty);
 			set => SetValue(IconProperty, value);
 		}
 
@@ -94,7 +94,7 @@ namespace devTools.WiXComponents.Controls
 		private static void OnIconRequestedChanged(DependencyObject sender, DependencyPropertyChangedEventArgs args)
 		{
 			UACShieldButton button = (UACShieldButton)sender;
-			button._image.Visibility = (bool)args.NewValue && !WindowsIdentityHelper.HasElevatedPrivileges()
+			button._image.Visibility = (bool)args.NewValue && !WindowsIdentityHelper.HasElevatedPrivileges
 									? Visibility.Visible
 									: Visibility.Collapsed;
 		}
@@ -103,7 +103,7 @@ namespace devTools.WiXComponents.Controls
 		{
 			UACShieldButton button = (UACShieldButton)sender;
 			button._image.Source = args.NewValue as ImageSource;
-			button._image.Visibility = (bool)sender.GetValue(IconRequestedProperty) && !WindowsIdentityHelper.HasElevatedPrivileges()
+			button._image.Visibility = (bool)sender.GetValue(IconRequestedProperty) && !WindowsIdentityHelper.HasElevatedPrivileges
 											? Visibility.Visible
 											: Visibility.Collapsed;
 		}
